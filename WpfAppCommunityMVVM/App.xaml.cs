21d47568295048b05c8acb9ebb4b62d82a7a4c41
SPYROS\Spyros <sponaris@gmail.com>
2025-06-10 09:56:57 +0300

    Add Dependency Injection support

--- WpfAppCommunityMVVM/App.xaml.cs
+++ WpfAppCommunityMVVM/App.xaml.cs
@@ -1,14 +1,52 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
 
-namespace WpfAppCommunityMVVM
+using WpfAppCommunityMVVM.Repository;
+using WpfAppCommunityMVVM.ViewModel;
+
+namespace WpfAppCommunityMVVM;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public static  IHost AppHost { get; private set; } = null!;
+
+    public App()
     {
+        AppHost = Host.CreateDefaultBuilder()
+                       .ConfigureServices((context, services) =>
+                       {
+                           // Register services
+                           services.AddSingleton<ICustomerRepository, InMemoryCustomerRepository>();
+
+                           // Register viewmodels
+                           services.AddTransient<CustomerGridViewModel>();
+
+                           // Register windows
+                           services.AddTransient<MainWindow>();
+                       })
+                       .Build();
     }
 
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        AppHost.Start();
+        
+        // Resolve and show the main window
+        var mainWindow = AppHost.Services.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        base.OnExit(e);
+        AppHost.StopAsync().GetAwaiter().GetResult();
+        AppHost.Dispose();
+    }
 }
+
+
--- WpfAppCommunityMVVM/MainWindow.xaml.cs
+++ WpfAppCommunityMVVM/MainWindow.xaml.cs
@@ -1,31 +1,18 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Windows;
 
-using System.Collections.ObjectModel;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-using WpfAppCommunityMVVM.Repository;
 using WpfAppCommunityMVVM.ViewModel;
 
-namespace WpfAppCommunityMVVM
+namespace WpfAppCommunityMVVM;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow(CustomerGridViewModel viewModel)
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-            DataContext = new CustomerGridViewModel(new InMemoryCustomerRepository());
-        }
+        InitializeComponent();
+
+        DataContext = viewModel;
     }
 }
