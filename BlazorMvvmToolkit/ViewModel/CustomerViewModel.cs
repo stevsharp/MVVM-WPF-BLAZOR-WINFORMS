29ac1f605294e04b5ba38171c3271db33ae47364
SPYROS\Spyros <sponaris@gmail.com>
2025-06-23 11:47:44 +0300

    Use CommunityToolkit.Mvvm in blazor

---
+++ BlazorMvvmToolkit/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace BlazorMvvmToolkit.ViewModel;
+
+public partial class CounterViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private int count;
+
+    [RelayCommand]
+    private void Increment() => count++;
+}
